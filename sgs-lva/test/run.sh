9a831150fcf2c2d1b51db058e3da09dafa202e5a
Peredo Andrade <operedo@gaudi.ac.upc.es>
2019-01-20 23:40:51 +0100

    Adaptation for Gaudi execution

--- sgs-lva/test/run.sh
+++ sgs-lva/test/run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+source /Soft/intelf/composer_xe_2013.3.163/bin/compilervars.sh ia32
+
 TEST=45_0_0_10_1 
 
+cp ../bin/SGS_LVA $TEST/
+cp ../../Boost_dijkstra/Boost_dijkstra $TEST/
 cd $TEST
-SGS_LVA sgs_lva.par > output.txt 2> error.txt
+./SGS_LVA sgs_lva.par > output.txt 2> error.txt
 RES="`perl absDiff.pl sgs.out sgs.out_ok | wc -l`"
 if [ $RES -eq 0 ]; then
 	echo "[sgs-lva] $TEST: PASSED"
