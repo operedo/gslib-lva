277a849264c2ac4d89933359f44f55149e929bb3
Peredo Andrade <operedo@arvei-69.ac.upc.es>
2019-01-30 05:15:16 +0100

    More scripts

--- sgs-lva/test/run.sh
+++ sgs-lva/test/run.sh
@@ -2,16 +2,17 @@
 
 source /Soft/intelf/composer_xe_2013.3.163/bin/compilervars.sh ia32
 
-TEST=45_0_0_10_1 
+#TEST=45_0_0_10_1 
+TEST=swiss-roll
 
 cp ../bin/SGS_LVA $TEST/
 cp ../../Boost_dijkstra/Boost_dijkstra $TEST/
 cd $TEST
 ./SGS_LVA sgs_lva.par > output.txt 2> error.txt
-RES="`perl absDiff.pl sgs.out sgs.out_ok | wc -l`"
-if [ $RES -eq 0 ]; then
-	echo "[sgs-lva] $TEST: PASSED"
-else
-	echo "[sgs-lva] $TEST: NOT PASSED. Compare output file ${TEST}/sgs.out with baseline file ${TEST}/sgs.out_ok"
-fi
+#RES="`perl absDiff.pl sgs.out sgs.out_ok | wc -l`"
+#if [ $RES -eq 0 ]; then
+#	echo "[sgs-lva] $TEST: PASSED"
+#else
+#	echo "[sgs-lva] $TEST: NOT PASSED. Compare output file ${TEST}/sgs.out with baseline file ${TEST}/sgs.out_ok"
+#fi
 cd ..	
--- sgs-lva/test/run_sge.sh
+++ sgs-lva/test/run_sge.sh
@@ -13,7 +13,8 @@
 
 source /Soft/intelf/composer_xe_2013.3.163/bin/compilervars.sh ia32
 
-TEST=45_0_0_10_1 
+#TEST=45_0_0_10_1 
+TEST=swiss-roll
 cd $TEST
 #cp /homeA/o/operedo/gslib-lva/sgs-lva/bin/SGS_LVA /homeA/o/operedo/gslib-lva/sgs-lva/test/$TEST/
 #cp /homeA/o/operedo/gslib-lva/Boost_dijkstra/Boost_dijkstra /homeA/o/operedo/gslib-lva/sgs-lva/test/$TEST/
@@ -23,10 +24,10 @@ cd $TEST
 
 #cd /scratch/nas/4/operedo/$TEST
 ./SGS_LVA sgs_lva.par > output.txt 2> error.txt
-RES="`perl absDiff.pl sgs.out sgs.out_ok | wc -l`"
-if [ $RES -eq 0 ]; then
-	echo "[sgs-lva] $TEST: PASSED"
-else
-	echo "[sgs-lva] $TEST: NOT PASSED. Compare output file ${TEST}/sgs.out with baseline file ${TEST}/sgs.out_ok"
-fi
+#RES="`perl absDiff.pl sgs.out sgs.out_ok | wc -l`"
+#if [ $RES -eq 0 ]; then
+#	echo "[sgs-lva] $TEST: PASSED"
+#else
+#	echo "[sgs-lva] $TEST: NOT PASSED. Compare output file ${TEST}/sgs.out with baseline file ${TEST}/sgs.out_ok"
+#fi
 cd -	
